7225cf57b36acfa46fb29822ab862ecc4b5b33ce
Yu Liang <yuli0316@bu.edu>
2024-12-01 19:28:16 -0500

    v1
    v1

--- src/db/ColumnStats.cpp
+++ src/db/ColumnStats.cpp
@@ -1,17 +1,137 @@
 #include <db/ColumnStats.hpp>
+#include <stdexcept>
+#include <cmath>
 
-using namespace db;
+namespace db {
 
 ColumnStats::ColumnStats(unsigned buckets, int min, int max)
-// TODO pa4: some code goes here
-{
-  // TODO pa4: some code goes here
+    : buckets_(buckets), min_(min), max_(max), total_count_(0) {
+  if (min >= max) {
+    throw std::invalid_argument("Invalid range for ColumnStats: min must be less than max");
+  }
+  bucket_width_ = static_cast<double>((max + 1) - min) / buckets_;
+  histogram_.resize(buckets_, 0);
 }
 
 void ColumnStats::addValue(int v) {
-  // TODO pa4: some code goes here
+  if (v < min_ || v > max_) {
+    return;
+  }
+  int bucketIndex = static_cast<int>((v - min_) / bucket_width_);
+  if (bucketIndex >= static_cast<int>(buckets_)) {
+    bucketIndex = buckets_ - 1;
+  }
+  histogram_[bucketIndex]++;
+  total_count_++;
 }
 
 size_t ColumnStats::estimateCardinality(PredicateOp op, int v) const {
-  // TODO pa4: some code goes here
+  if (total_count_ == 0) {
+    return 0;
+  }
+
+  if (v < min_) {
+    if (op == PredicateOp::LT || op == PredicateOp::LE) {
+      return 0;
+    }
+    return total_count_;
+  }
+  if (v > max_) {
+    if (op == PredicateOp::GT || op == PredicateOp::GE) {
+      return 0;
+    }
+    return total_count_;
+  }
+
+  int bucketIndex = static_cast<int>((v - min_) / bucket_width_);
+  if (bucketIndex >= static_cast<int>(buckets_)) {
+    bucketIndex = buckets_ - 1;
+  }
+
+  size_t count = 0;
+  double bucketRangeStart = min_ + bucketIndex * bucket_width_;
+  double bucketRangeEnd = bucketRangeStart + bucket_width_;
+  double bucketFraction;
+
+
+  switch (op) {
+    case PredicateOp::EQ: {
+      if (bucket_width_ < 1.0) {
+        count = histogram_[bucketIndex];
+      } else {
+        bucketFraction = 1.0 / bucket_width_;
+        count = static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+      }
+      break;
+    }
+
+    case PredicateOp::NE: {
+      if (bucket_width_ < 1.0) {
+        size_t eqCount = histogram_[bucketIndex];
+        count = total_count_ - eqCount;
+      } else {
+        bucketFraction = 1.0 / bucket_width_;
+        size_t eqCount = static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+        count = total_count_ - eqCount;
+      }
+      break;
+    }
+
+    case PredicateOp::LT: {
+      for (int i = 0; i < bucketIndex; ++i) {
+        count += histogram_[i];
+      }
+      bucketFraction = (v - bucketRangeStart) / bucket_width_;
+      count += static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+      break;
+    }
+
+    case PredicateOp::LE: {
+      for (int i = 0; i < bucketIndex; ++i) {
+        count += histogram_[i];
+      }
+      if (bucket_width_ < 1.0) {
+        count += histogram_[bucketIndex];
+      } else {
+        bucketFraction = (v - bucketRangeStart + 1) / bucket_width_;
+        count += static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+      }
+      break;
+    }
+
+  case PredicateOp::GT: {
+      if (bucket_width_ < 1.0) {
+        for (int i = bucketIndex+1; i < static_cast<int>(buckets_); ++i) {
+          count += histogram_[i];
+        }
+      } else {
+        bucketFraction = (bucketRangeEnd - v-1) / bucket_width_;
+        count += static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+        for (int i = bucketIndex + 1; i < static_cast<int>(buckets_); ++i) {
+          count += histogram_[i];
+        }
+      }
+      break;
+  }
+
+
+  case PredicateOp::GE: {
+    if (bucket_width_ < 1.0) {
+      count += histogram_[bucketIndex];
+    } else {
+      bucketFraction = (bucketRangeEnd - v) / bucket_width_;
+      count += static_cast<size_t>(histogram_[bucketIndex] * bucketFraction);
+    }
+    for (int i = bucketIndex + 1; i < static_cast<int>(buckets_); ++i) {
+      count += histogram_[i];
+    }
+    break;
+  }
+
+  default:
+    throw std::runtime_error("Unsupported PredicateOp in estimateCardinality");
+  }
+  return count;
 }
+
+} // namespace db
--- src/db/include/db/ColumnStats.hpp
+++ src/db/include/db/ColumnStats.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <db/Query.hpp>
+#include <vector>
 
 namespace db {
 
@@ -8,42 +9,49 @@ namespace db {
  * A class to represent a fixed-width histogram over a single integer-based field.
  */
 class ColumnStats {
-  // TODO pa4: add private members
+private:
+ unsigned buckets_;              // Number of buckets
+ int min_;                       // Minimum value of the range
+ int max_;                       // Maximum value of the range
+ double bucket_width_;           // Width of each bucket
+ std::vector<size_t> histogram_; // Vector to store counts for each bucket
+ size_t total_count_;            // Total number of values added to the histogram
 
 public:
-  /**
-   * Create a new ColumnStats.
-   *
-   * This ColumnStats should maintain a histogram of integer values that it receives.
-   * It should split the histogram into buckets.
-   *
-   * The values that are being processed will be provided one-at-a-time through the "addValue()" function.
-   *
-   * Your implementation should use constant space and have constant execution time with respect to
-   * the number of values being added (i.e., you store the values that you add in the histogram).
-   *
-   * @param buckets The number of buckets to split the input range.
-   * @param min The minimum integer value that this instance will process
-   * @param max The maximum integer value that this instance will process
-   */
-  ColumnStats(unsigned buckets, int min, int max);
+ /**
+  * Create a new ColumnStats.
+  *
+  * This ColumnStats should maintain a histogram of integer values that it receives.
+  * It should split the histogram into buckets.
+  *
+  * The values that are being processed will be provided one-at-a-time through the "addValue()" function.
+  *
+  * Your implementation should use constant space and have constant execution time with respect to
+  * the number of values being added (i.e., you store the values that you add in the histogram).
+  *
+  * @param buckets The number of buckets to split the input range.
+  * @param min The minimum integer value that this instance will process
+  * @param max The maximum integer value that this instance will process
+  */
+ ColumnStats(unsigned buckets, int min, int max);
 
-  /**
-   * Add a value to the histogram.
-   * @param v Value to add to the histogram
-   */
-  void addValue(int v);
+ /**
+  * Add a value to the histogram.
+  * @param v Value to add to the histogram
+  */
+ void addValue(int v);
 
-  /**
-   * Estimate the selectivity of a particular predicate and operand on this table.
-   *
-   * For example, if "op" is "GT" and "v" is 5,
-   * return your estimate of the fraction of elements that are greater than 5.
-   *
-   * @param op Operator
-   * @param v Value
-   * @return Predicted selectivity of this particular operator and value
-   */
-  size_t estimateCardinality(PredicateOp op, int v) const;
+ /**
+  * Estimate the selectivity of a particular predicate and operand on this table.
+  *
+  * For example, if "op" is "GT" and "v" is 5,
+  * return your estimate of the fraction of elements that are greater than 5.
+  *
+  * @param op Operator
+  * @param v Value
+  * @return Predicted selectivity of this particular operator and value
+  */
+ size_t estimateCardinality(PredicateOp op, int v) const;
 };
+
 } // namespace db
